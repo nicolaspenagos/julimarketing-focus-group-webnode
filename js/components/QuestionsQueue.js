c31e9507190e3e6f32eccdd8d14627aa65a4dc20
nicolaspenagos <nicolas.penagosm98@gmail.com>
2020-11-30 04:40:30 -0500

    All functions are almost done, there is a missing error with delete button of the main question

---
+++ js/components/QuestionsQueue.js
@@ -0,0 +1,42 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author Nicolás Penagos Montoya
+ * nicolas.penagosm98@gmail.com
+ **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
+ class QuestionsQueue{
+
+    constructor(question){
+        this.question = question;
+    }
+
+    render =()=>{
+
+        let component = document.createElement('div');
+        component.className = 'question';
+
+        let questionDiv = document.createElement('div');
+        questionDiv.className = 'questionDiv';
+        questionDiv.innerHTML = this.question.q;
+
+        let scoreDiv = document.createElement('div');
+        scoreDiv.className = 'scoreDiv';
+        scoreDiv.innerHTML = this.question.s;
+
+        let deleteButton = document.createElement('button');
+        deleteButton.className = 'deleteButton';
+        deleteButton.innerHTML = 'X';
+
+        deleteButton.addEventListener('click', ()=>{
+            let reference = database.ref('history/'+this.question.id).set(null);
+        });
+
+        component.appendChild(questionDiv);
+        component.appendChild(scoreDiv);
+        component.appendChild(deleteButton);
+
+        return component;
+
+    }
+
+ }
--- js/index.js
+++ js/index.js
@@ -14,7 +14,16 @@ const database = firebase.database();
 // -------------------------------------
 const questionInput = document.getElementById('currentQuestionInput');
 const okButton = document.getElementById('okButton');
-const currentQuestionDiv = document.getElementById('currentQuestionDiv');
+const currentQuestionDiv = document.getElementById('currentQuestion');
+const averageDiv = document.getElementById('average');
+const historyDiv = document.getElementById('history');
+const deleteButton = document.getElementById('delete');
+
+// -------------------------------------
+// GLOBAL VARIABLES
+// -------------------------------------
+var currentQuestionString;
+var currentQuestionAverage;
 
 // -------------------------------------
 // EVENTS
@@ -24,12 +33,22 @@ okButton.addEventListener('click', ()=>{
     let questionS = questionInput.value;
     if(questionS != null && questionS != ''){
 
-        let reference = database.ref('currentQuestion');
+        let historyReference = database.ref('history/').push();
+        let historyQuestion = {
+            id: historyReference.key,
+            q: currentQuestionString,
+            s: currentQuestionAverage
+        }
+        historyReference.set( historyQuestion);
+
+        let reference = database.ref('currentQuestion/question');
+        let scoresReference = database.ref('currentQuestion/scores');
         let question = {
             question: questionS
         }
 
         reference.set(question);
+        scoresReference.set(null);
 
     }else{
         alert('The new question cannot be empty');
@@ -39,21 +58,93 @@ okButton.addEventListener('click', ()=>{
     
 });
 
+deleteButton.addEventListener('click', ()=>{
+
+    let historyReference = database.ref('history/').push();
+    let historyQuestion = {
+        id: historyReference.key,
+        q: currentQuestionString,
+        s: currentQuestionAverage
+    }
+    historyReference.set( historyQuestion);
+
+    let reference = database.ref('currentQuestion/question');
+    let scoresReference = database.ref('currentQuestion/scores');
+    let question = {
+        question: 'NO_QUESTION'
+    }
+
+    reference.set(question);
+    scoresReference.set(null);
+
+});
 
 // -------------------------------------
 // DATA READING
 // -------------------------------------
-database.ref('currentQuestion').on('value', function(data){
+database.ref('currentQuestion/question').on('value', function(data){
 
     data.forEach(
         currentQuestion => {
             
             let val = currentQuestion.val();
-            currentQuestionDiv.innerHTML = val;
+            if(val === 'NO_QUESTION'){
+                currentQuestionDiv.innerHTML = '';
+            }else{
+
+                currentQuestionDiv.innerHTML = val;
+                currentQuestionString = val;
+
+            }
+
+            currentQuestionAverage = 0; 
          
         }
     );
 
 });
+
+database.ref('currentQuestion/scores').on('value', function(data){
+
+    let summatory = 0;
+    let counter = 0;
+
+    data.forEach(
+        score => {
+            
+            let val = score.val().score;
+            summatory+= val;
+            counter += 1;
+           
+        }
+    );
+
+    if(counter!= 0 ){
+
+        let average = summatory/counter;
+        averageDiv.innerHTML = '' + average;
+        currentQuestionAverage = average;
+
+    }else{
+        averageDiv.innerHTML = '   ';
+    }
+   
+});
+
+database.ref('history').on('value', function(data){
+    historyDiv.innerHTML = '';
+    data.forEach(
+        currentQuestion => {
+            
+            let val = currentQuestion.val();
+            let questionComponent = new QuestionsQueue(val);
+            historyDiv.appendChild(questionComponent.render());
+            console.log(val.q);
+          
+        }
+    );
+
+});
+
 
 
